8b74f3333c31779423b5451e23cfbac8fa1837d1
rumitv <157432863+rumitv@users.noreply.github.com>
2024-09-02 23:13:28 +0000

    9/2

--- program0_Maryam_Fathey.c
+++ program0_Maryam_Fathey.c
@@ -18,7 +18,7 @@
 int main()
 {
  
-    // prints the message to the command line.
+    // prints the message to the command line..
     printf("The current month is August.\nThe current year is 2024.\nI am a student at UCF.\nThis Fall 2024 I am taking Intro to Programming in C with Benji.\nI am super exicted to learn many things this semester!\n");
  
     return 0;
